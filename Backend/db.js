35d22512646edfae865d48fa1875eac273cb06ed
Ulfric Irons <ironsulfric@gmail.com>
2026-06-02 11:15:11 +0100

    Frontend Scripting Logic

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection(.{
+const connection = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"password",
---
+++ Frontend/script.js
@@ -0,0 +1,77 @@
+const API_URL = 'http://localhost:3000/books';
+
+loadBooks();
+
+function loadBooks() {
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(data=>{  
+    const bookList = document.getElementById('bookList');
+    bookList.innerHTML = '';
+    data.forEach(book => {
+        bookList.innerHTML += `
+        <div class="book-card">
+            <img src="$(book.image)">
+            <h3>${book.title}</h3>
+            <p>Author: ${book.author}</p>
+            <p>Category: ${book.category}</p>
+            <p>$${book.price}</p>
+            <button
+                class"delete-btn"
+                onclick="deleteBook(${book.id})">Delete
+            </button>
+        </div>
+        `;
+    });
+});}
+
+function addBook() {
+    const book = {
+        title: document.getElementById('title').value,
+        author: document.getElementById('author').value,
+        category: document.getElementById('category').value,
+        price: parseFloat(document.getElementById('price').value),
+        image: document.getElementById('image').value
+    };
+    fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(book)
+    })
+    .then(response => response.json())
+    .then(() => {
+        loadBooks();
+    });
+};
+
+function deleteBook(id) {
+    fetch(`${API_URL}/${id}`, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(() => {
+        loadBooks();
+    });
+};
+
+function searchBooks() {
+    const keyword = document.getElementByIld("search").value;
+    fetch('http://localhost:3000/books/search/${keyword}').then(response => response.json())
+    .then(data => {
+        const bookList = document.getElementById('bookList');
+        bookList.innerHTML = '';
+        data.forEach(book => {
+            bookList.innerHTML += `
+            <div class="book-card">
+                <img src="${book.image}">
+                <h3>${book.title}</h3>
+                <p>Author: ${book.author}</p>
+                <p>Category: ${book.category}</p>
+                <p>$${book.price}</p>
+            </div>
+            `;
+        });
+    });
+};
